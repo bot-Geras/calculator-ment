d8d5671ad403a55f0874c6f7f7b0d064661ba0d4
Brian <briandave771@gmail.com>
2022-12-10 02:52:26 +0300

    Add clear function  to clean the display

--- calculator.js
+++ calculator.js
@@ -4,13 +4,12 @@ const display = calculator.querySelector(".display-evaluation");
 
 keys.addEventListener("click", (e) => {
   if (!e.target.closest("button")) return;
-
   const key = e.target;
   const keyValue = key.textContent;
   const displayValue = display.textContent;
   const type = key.dataset.type;
   const { previousKeyType } = calculator.dataset;
-  // is this a no. key
+
   if (type === "number") {
     if (displayValue === "0" || previousKeyType === "operator") {
       display.textContent = keyValue;
@@ -19,42 +18,29 @@ keys.addEventListener("click", (e) => {
     }
   }
 
-  // is this an operator key?
-
   if (type === "operator") {
     const operatorKeys = keys.querySelectorAll(`[data-type="operator"]`);
     operatorKeys.forEach((el) => (el.dataset.state = ""));
-    // console.log(operatorKeys);
+
     key.dataset.state = "selected";
     calculator.dataset.firstNumber = displayValue;
     calculator.dataset.operator = key.dataset.key;
   }
   if (type === "equal") {
-    // perform a calculations
     const firstNumber = calculator.dataset.firstNumber;
     const operator = calculator.dataset.operator;
     const secondNumber = displayValue;
-    // console.log(firstNumber, operator, secondNumber);
 
     display.textContent = calculate(firstNumber, operator, secondNumber);
   }
+
+  if (type === "clear") {
+    display.textContent = "0";
+    delete calculator.dataset.firstNumber;
+    delete calculator.dataset.operator;
+  }
   calculator.dataset.previousKeyType = type;
-
-  //   console.log(typeof keyValue);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function calculate(firstNumber, operator, secondNumber) {
   firstNumber = parseInt(firstNumber);
@@ -66,12 +52,11 @@ function calculate(firstNumber, operator, secondNumber) {
   if (operator === "divide") result = firstNumber / secondNumber;
   console.log(result);
 
-// switch(operator) {
-//     case "plus": result = firstNumber + secondNumber;
-//     case "minus": result = firstNumber - secondNumber;
-//     case "times": result = firstNumber * secondNumber;
-//     case "divide": result = firstNumber / secondNumber;
-// }
+  // switch(operator) {
+  //     case "plus": result = firstNumber + secondNumber;
+  //     case "minus": result = firstNumber - secondNumber;
+  //     case "times": result = firstNumber * secondNumber;
+  //     case "divide": result = firstNumber / secondNumber;
+  // }
   return result;
- 
 }
