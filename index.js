4b4f6aebffdb84f3df4ad6bd67272dacc6303ebe
Brian <briandave771@gmail.com>
2022-12-09 14:26:57 +0300

    Add functionality to the calculator

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const screenDisplay = document.querySelector("#evaluation");
+const buttons = document.querySelectorAll("button");
 
+let calculations = [];
+let accumulatveCalculation;
+
+function calculate(button) {
+  const value = button.textContent;
+  if (value === "CLEAR") {
+    calculations = [];
+    screenDisplay.textContent = ".";
+  } else if (value === "=") {
+    console.log(accumulatveCalculation);
+    screenDisplay.textContent = eval(accumulatveCalculation);
+  } else {
+    calculations.push(value);
+    accumulatveCalculation = calculations.join("");
+    screenDisplay.textContent = accumulatveCalculation;
+  }
+}
+
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    calculate(button);
+  })
+);
